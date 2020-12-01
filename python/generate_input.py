de947510c9508532ebb454239ae13c3e214e33e2
Nita Eduard <nitaeduarddd@gmail.com>
2020-12-01 06:12:19 -0800

    Fix horizontal and domain bug

--- python/generate_input.py
+++ python/generate_input.py
@@ -1,9 +1,9 @@
 import csv
-bigW = 4
-bigH = 8
-numRectangles = 4
-w = [2, 2, 2, 2]
-h = [3, 3, 3, 3]
+bigW = 3
+bigH = 3
+numRectangles = 3
+w = [3, 2, 1]
+h = [1, 1, 1]
 
 with open('data_problem.txt') as csv_file:
 	csv_reader = csv.reader(csv_file, delimiter=',')
@@ -20,13 +20,12 @@ with open('data_problem.txt') as csv_file:
 				rect_in.write("x0,y0,width,height\n")
 				rect_in.write(",,"+row[0]+","+row[1]+"\n")
 		else:
-			print(line_count)
 			w.append(int(row[0]))
 			h.append(int(row[1]))
 		line_count+=1
 
 file = open("test.in", "w")
-file.write(f"""assign(domain_size, {max(bigW, bigH)}).
+file.write(f"""assign(domain_size, {max(bigW, bigH)+1}).
 assign(max_models, 1).
 set(arithmetic).
 
@@ -34,10 +33,12 @@ formulas(rectangles).
 """)
 for i in range(numRectangles):
 	file.write(f"""
-	r{i}h <-> (L{i} = {w[i]} & H{i} = {h[i]}).
-	r{i}v <-> (L{i} = {h[i]} & H{i} = {w[i]}).
-	r{i}v | r{i}h.
-	""")
+	R{i}v <-> (L{i} = {h[i]} & H{i} = {w[i]}).
+	R{i}h <-> (L{i} = {w[i]} & H{i} = {h[i]}).
+ 	(L{i} != H{i}) -> (-(R{i}v -> R{i}h) | -(R{i}h -> R{i}v)).
+ 	(L{i} = H{i}) -> R{i}v.	
+ 		""")
+
 file.write(f"""
 end_of_list.
 
@@ -45,8 +46,8 @@ formulas(bounds).
 """)
 for i in range(numRectangles):
 	file.write(f"""
-	X{i} >= 0 & (X{i} + L{i} <= {bigW}).
 	Y{i} >= 0 & (Y{i} + H{i} <= {bigH}).
+	X{i} >= 0 & (X{i} + L{i} <= {bigW}).
 	""")
 file.write(f"""
 end_of_list.
@@ -54,10 +55,10 @@ end_of_list.
 formulas(intersections).
 """)
 for i in range(numRectangles):
-	for j in range(numRectangles):
-		if i != j and j > i:
+	for j in range(i, numRectangles):
+		if i != j:
 			file.write(f"""
-	X{i} + L{i} <= X{j} | X{j} + L{j} <= X{i} | Y{i} + H{i} <= Y{j} | Y{j} + H{j} <= Y{i}.
+	(X{i} + L{i} <= X{j}) | (X{j} + L{j} <= X{i}) | (Y{i} + H{i} <= Y{j}) |(Y{j} + H{j} <= Y{i}).
 """)
 file.write(f"""
 end_of_list.
